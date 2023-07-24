5421cc450a790519c3ac0f0d1608bc61814ffba4
김학규 <zage4448@gmail.com>
2023-07-24 23:05:42 +0900

    feat: [김학규] 항공권 검색 로딩 form 구현 [TGV-28]

--- src/components/flight/FlightProductListLoadingForm.vue
+++ src/components/flight/FlightProductListLoadingForm.vue
@@ -1,13 +1,65 @@
-<template lang="">
+<template>
     <div>
-        loading
+      <div class="flight_loading_background"></div>
+      <div class="loading-bar-container">
+        <div class="loading-bar" :class="{ 'complete': isLoadingComplete }"></div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isLoadingComplete: false,
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.isLoadingComplete = true;
+      }, 10000); // 10초 후에 isLoadingComplete 값을 true로 변경
+    },
+  };
+  </script>
+  
+  <style>
+  .loading-bar-container {
+    width: 100%;
+    height: 20px;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+    border-radius: 4px;
+    overflow: hidden;
+    position: absolute;
+    margin-top: 100px;
+  }
+  
+  .loading-bar {
+    height: 100%;
+    background-color: #4CAF50;
+    animation: moveBar 10s linear;
+  }
+  .flight_loading_background{
+    position: absolute;
+    margin:0;
+    background-image: url("@/assets/images/flight_loading_background.jpg");
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 1000px;
+  }
+  
+  .complete {
+    animation: none;
+  }
+  
+  @keyframes moveBar {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+  </style>
+  
