9bb883b2bade236c4f177c3685cc7e85f0860560
김학규 <zage4448@gmail.com>
2023-07-24 22:20:13 +0900

    fix: [김학규] fastapi로 전송하는 정보 명칭 오류 fix [TGV-25]

--- src/views/flight/FlightProductListPage.vue
+++ src/views/flight/FlightProductListPage.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <div>
-      <FlightReserveForm @submit="searchFlights"
-          :departureAirport="flightSearch.departureAirport"
-          :arrivalAirport="flightSearch.arrivalAirport"
-          :passengers="flightSearch.passengers"
-          :departureDate="flightSearch.departureDate"
-          :returnDate="flightSearch.returnDate"
-          :oneWayOnly="flightSearch.nonStop"
-          :roundTrip="flightSearch.roundTrip"/>
-    </div>
-    <div>
-      <FlightProductListForm/>
+    <div style="width: 90%; margin-left: auto; margin-right: auto;">
+      <div>
+        <FlightReserveForm @submit="searchFlights"
+            :departureAirport="flightSearch.departureAirport"
+            :arrivalAirport="flightSearch.arrivalAirport"
+            :passengers="flightSearch.passengers"
+            :departureDate="flightSearch.departureDate"
+            :returnDate="flightSearch.returnDate"
+            :oneWayOnly="flightSearch.nonStop"
+            :roundTrip="flightSearch.roundTrip"/>
+      </div>
+      <div>
+        <FlightProductListForm/>
+      </div>
     </div>
   </div>
 </template>
@@ -32,16 +34,15 @@ export default {
     ...mapActions(flightModule, ['requestFlightProductsToFastAPI']),
 
     async searchFlights(payload) {
-      const {
-        originLocationCode,
-        destinationLocationCode,
-        departureDate,
-        returnDate,
-        nonStop,
-        adults,
-        children,
-        infants
-      } = payload
+      const originLocationCode = payload.departureAirport.code
+      const destinationLocationCode = payload.arrivalAirport.code
+      const departureDate = payload.departureDate
+      const returnDate = payload.returnDate
+      const nonStop = payload.oneWayOnly
+      const adults = payload.passengers.adult
+      const children = payload.passengers.child
+      const infants = payload.passengers.infant
+
       await this.requestFlightProductsToFastAPI({
         originLocationCode,
         destinationLocationCode,
